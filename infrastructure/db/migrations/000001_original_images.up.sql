adad227ad52f887e5c7d834d4a9234c37d4b0837
yuita-yoshihiko <soup998211@gmail.com>
2023-10-20 23:50:46 +0900

    修正

--- infrastructure/db/migrations/000001_original_images.up.sql
+++ infrastructure/db/migrations/000001_original_images.up.sql
@@ -1,6 +1,5 @@
 CREATE TABLE original_images (
     id SERIAL PRIMARY KEY,
     object_name VARCHAR(255) NOT NULL,
-    error boolean NOT NULL DEFAULT false,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
--- infrastructure/db/migrations/000003_converted_images.up.sql
+++ infrastructure/db/migrations/000003_converted_images.up.sql
@@ -1,6 +1,5 @@
 CREATE TABLE converted_images (
     id SERIAL PRIMARY KEY,
     converted_object_name VARCHAR(255),
-    error boolean NOT NULL DEFAULT false,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
