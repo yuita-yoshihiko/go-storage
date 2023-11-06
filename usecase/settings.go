e6d12c695477278a9850b426c527f00b145bc6df
yuita-yoshihiko <soup998211@gmail.com>
2023-11-06 22:19:08 +0900

    DBから取得した情報を直接構造体にスキャンするように修正

--- usecase/settings.go
+++ usecase/settings.go
@@ -16,16 +16,8 @@ type ImageConversionSetting struct {
 func GetConversionSettings(db *sql.DB, id int) (*ImageConversionSetting, error) {
 	query := `SELECT output_format, width_resize_ratio, height_resize_ratio FROM image_conversion_settings WHERE id = $1`
 	setting := &ImageConversionSetting{}
-
-	var format string
-	var width, height float64
-	if err := db.QueryRow(query, id).Scan(&format, &width, &height); err != nil {
+	if err := db.QueryRow(query, id).Scan(&setting.OutputFormat, &setting.WidthResizeRatio, &setting.HeightResizeRatio); err != nil {
 		return nil, err
 	}
-
-	setting.OutputFormat = format
-	setting.WidthResizeRatio = float64(width)
-	setting.HeightResizeRatio = float64(height)
-
 	return setting, nil
 }
